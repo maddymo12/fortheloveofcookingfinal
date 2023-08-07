0ad239c38ed9e06a5d5cd66854a638edab610b8d
maddymo12 <139657054+maddymo12@users.noreply.github.com>
2023-08-06 23:04:11 -0600

    Add files via upload

---
+++ welcome.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+
+Welcome <?php echo $_POST["name"]; ?><br>
+    
+Your email address is: <?php echo $_POST["email"]; ?><br>
+    
+    What Kind Of Recipie Is It:<?php echo $_POST["What Kind Of Recipie Is It"]; ?><br>
+    
+    May We Add Alterations for Cost/Allergens:<?php echo $_POST["May We Add Alterations for Cost/Allergens"]; ?><br>
+
+</body>
+</html> 
